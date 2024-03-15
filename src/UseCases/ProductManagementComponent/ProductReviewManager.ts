12820966bd4ceec3e0fcd94cf52011d7fbf068e7
saugatdai <saugatdai@gmail.com>
2024-03-14 19:28:36 -0600

    ProductReviewManager Added

---
+++ src/UseCases/ProductManagementComponent/ProductReviewManager.ts
@@ -0,0 +1,51 @@
+import ProductReview from "../../entities/ProductComponent/ProductReview";
+
+export interface ProductReviewStorageInteractor {
+  createProductReview(
+    productReview: ProductReview | null
+  ): Promise<ProductReview>;
+  getProductReview(productReviewId: number): Promise<ProductReview>;
+  updateProductReview(
+    productReview: ProductReview | null
+  ): Promise<ProductReview>;
+  deleteProductReview(productReviewId: number): Promise<void>;
+}
+
+export default class ProductReviewManager {
+  constructor(
+    private _productReview: ProductReview | null,
+    private _storage: ProductReviewStorageInteractor
+  ) {}
+
+  private throwProductReviewNotSetError() {
+    if (!this._productReview) {
+      throw new Error("Product review not set");
+    }
+  }
+
+  public async createProductReview() {
+    this.throwProductReviewNotSetError();
+    this._productReview = await this._storage.createProductReview(
+      this._productReview
+    );
+  }
+
+  public async getProductReview(productReviewId: number) {
+    this._productReview = await this._storage.getProductReview(productReviewId);
+  }
+
+  public async updateProductReview() {
+    this.throwProductReviewNotSetError();
+    this._productReview = await this._storage.updateProductReview(
+      this._productReview
+    );
+  }
+
+  public async deleteProductReview() {
+    if (this._productReview) {
+      await this._storage.deleteProductReview(this._productReview.id);
+      this._productReview = null;
+    }
+  }
+  
+}
